56ef0fc2717c33b43de01eae43e23ffb0e93c7f1
sweetcola <838689894@qq.com>
2021-11-10 17:05:25 +0800

    refactor: "onBeforeRequest" and "onHeadersReceived" should not return promise

--- src/entry/background/web-request.ts
+++ src/entry/background/web-request.ts
@@ -8,13 +8,13 @@ const viewerURL = chrome.runtime.getURL('/pdf-viewer/web/viewer.html');
 
 const getRedirectURL = (url: string) => (`${viewerURL}?file=${encodeURIComponent(url)}`);
 
-const onBeforeRequest: (details: chrome.webRequest.WebRequestBodyDetails) => void = async ({ url, tabId }) => {
-    const { enablePdfViewer } = await getLocalStorageAsync<PickedOptions>(keys);
-
-    enablePdfViewer && tabId >= 0 && chrome.tabs.update(tabId, { url: getRedirectURL(url) });
+const onBeforeRequest: (details: chrome.webRequest.WebRequestBodyDetails) => void = ({ url, tabId }: chrome.webRequest.WebRequestBodyDetails) => {
+    getLocalStorageAsync<PickedOptions>(keys).then(({ enablePdfViewer }) => {
+        enablePdfViewer && chrome.tabs.update(tabId, { url: getRedirectURL(url) });
+    });
 };
 
-const onHeadersReceived: (details: chrome.webRequest.WebResponseHeadersDetails) => void = async ({ responseHeaders, url, method, tabId }) => {
+const onHeadersReceived: (details: chrome.webRequest.WebResponseHeadersDetails) => void = ({ responseHeaders, url, method, tabId }) => {
     if (method.toLowerCase() !== 'get' || !responseHeaders) { return; }
 
     const contentType = responseHeaders.find(({ name }) => (name.toLowerCase() === 'content-type'));
@@ -23,9 +23,9 @@ const onHeadersReceived: (details: chrome.webRequest.WebResponseHeadersDetails) 
 
     const value = contentType.value.toLowerCase();
     if (value.includes('application/pdf') || (value.includes('application/octet-stream') && url.toLowerCase().endsWith('.pdf'))) {
-        const { enablePdfViewer } = await getLocalStorageAsync<PickedOptions>(keys);
-
-        enablePdfViewer && tabId >= 0 && chrome.tabs.update(tabId, { url: getRedirectURL(url) });
+        getLocalStorageAsync<PickedOptions>(keys).then(({ enablePdfViewer }) => {
+            enablePdfViewer && chrome.tabs.update(tabId, { url: getRedirectURL(url) });
+        });
     }
 };
 
