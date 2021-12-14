f87957f4a790be536fee25452ecd7d84c104904b
sweetcola <838689894@qq.com>
2021-12-14 16:14:07 +0800

    chore: add component named "Checkbox" with material design

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import './style.css';
+
+type ChcekboxProps = {
+    label?: string | number | React.ReactElement;
+    checked?: boolean;
+    onChange?: (checked: boolean) => void;
+} & Pick<React.HtmlHTMLAttributes<HTMLInputElement>, 'className'>;
+
+// Will replace will React built-in "useId" while migrating to React-18.0.0 (stable).
+const useId = () => {
+    const id = useMemo(() => {
+        return Math.random().toString().substring(2);
+    }, []);
+
+    return id;
+};
+
+const Checkbox: React.FC<ChcekboxProps> = ({ label, checked, onChange }) => {
+    const [activing, setActiving] = useState(false);
+
+    const activedRef = useRef(false);
+    const checkboxRootRef = useRef<HTMLSpanElement>(null);
+
+    const id = useId();
+
+    useEffect(() => {
+        if (!activing) { return; }
+
+        const onMouseUp = () => {
+            setActiving(false);
+        };
+
+        window.addEventListener('mouseup', onMouseUp);
+
+        return () => window.removeEventListener('mouseup', onMouseUp);
+    }, [activing]);
+
+    return (
+        <label htmlFor={id} className='checkbox'>
+            <span
+                ref={checkboxRootRef}
+                className={`checkbox-root${activedRef.current ? activing ? ' checkbox--activation' : ' checkbox--deactivation' : ''}${checked ? ' checkbox--checked' : ''}`}
+                onMouseDown={() => {
+                    if (!checkboxRootRef.current) { return; }
+
+                    const target = checkboxRootRef.current;
+
+                    const { clientWidth, clientHeight } = target;
+
+                    const size = Math.floor(clientWidth * 0.6);
+                    const start = { x: clientWidth * 0.2, y: clientHeight * 0.2 };
+                    const end = { x: clientWidth * 0.2, y: clientHeight * 0.2 };
+
+                    target.setAttribute('style', `--ripple-size:${size}px;--ripple-translate-start:${start.x}px,${start.y}px;--ripple-translate-end:${end.x}px,${end.y}px;`);
+
+                    activedRef.current = true;
+                    setActiving(true);
+                }}
+            >
+                <input id={id} className='checkbox-input' onChange={e => onChange?.(e.target.checked)} type='checkbox' checked={checked} />
+                <svg className='iconfont' viewBox='0 0 24 24'>
+                    <path
+                        d={checked
+                            ? 'M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z'
+                            : 'M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z'
+                        }
+                    />
+                </svg>
+                <div className='ripple'></div>
+            </span>
+            {label && <span>{label}</span>}
+        </label>
+    );
+};
+
+export default Checkbox;
