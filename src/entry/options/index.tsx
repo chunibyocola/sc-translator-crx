6d62be5bbd748281521458d47b5ac0f002c9ebba
sweetcola <838689894@qq.com>
2022-02-14 16:13:29 +0800

    [Options page] Will not respond to the change of theme now
    With the preset Dark theme, options page doesn't look well.
    And it is hard to adjust the UIs' color while there is "customize theme".

--- src/entry/options/index.tsx
+++ src/entry/options/index.tsx
@@ -4,13 +4,10 @@ import Options from './Options';
 import './style.css';
 import '../../styles/global.css';
 import { getI18nMessage } from '../../public/chrome-call';
-import { appendColorVarsStyle } from '../../public/inject-style';
 import { getLocalStorageAsync } from '../../public/utils';
 import { DefaultOptions } from '../../types';
 import defaultOptions from '../../constants/defaultOptions';
 import { initOptions } from '../../public/options';
-
-appendColorVarsStyle(document.head);
 
 document.documentElement.id = 'sc-translator-root';
 document.title = `${getI18nMessage('optionsTitle')} - ${getI18nMessage('extName')}`;
