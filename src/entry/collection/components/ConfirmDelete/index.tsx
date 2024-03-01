5fbac827e6258f4d2b61e09908b4a5256b2a1056
sweetcola <838689894@qq.com>
2024-03-01 16:07:01 +0800

    [ConfirmDelete] Set `deleteList` as optional parameter

--- src/entry/collection/components/ConfirmDelete/index.tsx
+++ src/entry/collection/components/ConfirmDelete/index.tsx
@@ -11,7 +11,7 @@ type ConfirmDeleteProps = {
     onClose: () => void;
     textPair?: [string, string];
     drawerTitle: string;
-    deleteList: string[];
+    deleteList?: string[];
 };
 
 const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ onConfirm, onCancel, onClose, textPair, drawerTitle, deleteList }) => {
@@ -35,7 +35,7 @@ const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ onConfirm, onCancel, onCl
                         <div className='text-pair__first'>{textPair[0]}</div>
                         <div className='text-pair__second' title={textPair[1]}>{textPair[1]}</div>
                     </div>}
-                    <Button
+                    {deleteList ? <Button
                         variant='text'
                         onClick={() => {
                             setFold(!fold);
@@ -43,8 +43,10 @@ const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ onConfirm, onCancel, onCl
                     >
                         <span>{drawerTitle}</span>
                         <IconFont iconName='#icon-GoChevronDown' style={{minWidth: '1em', rotate: fold ? 'unset' : '180deg'}} />
-                    </Button>
-                    <div className='confirm-delete__content__list scrollbar' style={{display: fold ? 'none' : 'block'}}>
+                    </Button> : <div className='confirm-delete__drawer-title'>
+                        {drawerTitle}
+                    </div>}
+                    {deleteList && <div className='confirm-delete__content__list scrollbar' style={{display: fold ? 'none' : 'block'}}>
                         {deleteList.map((item) => (<div
                             className='confirm-delete__content__list_item'
                             title={item}
@@ -52,7 +54,7 @@ const ConfirmDelete: React.FC<ConfirmDeleteProps> = ({ onConfirm, onCancel, onCl
                         >
                             {item}
                         </div>))}
-                    </div>
+                    </div>}
                 </div>
                 <div className='confirm-delete__buttons'>
                     <Button
