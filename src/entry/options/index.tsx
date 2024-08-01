2a3f2615120101c13e88e5aa76a5a7ff60f2fdd3
sweetcola <838689894@qq.com>
2024-08-01 15:24:39 +0800

    Replace init

--- src/entry/options/index.tsx
+++ src/entry/options/index.tsx
@@ -3,17 +3,12 @@ import * as ReactDOMClient from 'react-dom/client';
 import Options from './Options';
 import './style.css';
 import '../../styles/global.css';
-import { getLocalStorageAsync } from '../../public/utils';
-import { DefaultOptions } from '../../types';
-import defaultOptions from '../../constants/defaultOptions';
-import { initOptions } from '../../public/options';
 import { getMessage } from '../../public/i18n';
+import scOptions from '../../public/sc-options';
 
 document.title = `${getMessage('optionsTitle')} - ${getMessage('extName')}`;
 
-getLocalStorageAsync<DefaultOptions>(Object.keys(defaultOptions) as (keyof DefaultOptions)[]).then((options) => {
-    initOptions(options);
-
+scOptions.init().then(() => {
     const rootElement = document.getElementById('root');
 
     rootElement && ReactDOMClient.createRoot(rootElement).render(
