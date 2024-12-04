0f7612806553fdeda26720e518693bd565041a87
sweetcola <838689894@qq.com>
2024-12-04 14:32:23 +0800

    Fix mojidict unavailable
    Closes #181

--- src/public/translate/mojidict/translate.ts
+++ src/public/translate/mojidict/translate.ts
@@ -4,12 +4,19 @@ import { TranslateParams } from '../translate-types';
 import { TranslateResult } from '../../../types';
 
 export const translate = async ({ text }: TranslateParams) => {
-    const url = `https://api.mojidict.com/parse/functions/search_v3`;
+    const url = `https://api.mojidict.com/parse/functions/union-api`;
 
     const fetchJson = {
-        searchText: text,
-        needWords :true,
-        _ApplicationId:"E62VyFVLMiW7kvbtVq3p"
+        functions: [
+          { name: 'search-all', params: { text, types: [102, 106, 103] } },
+          { name: 'mojitest-examV2-searchQuestion-v2', params: { text, limit: 1, page: 1 } },
+          { name: 'deconjugateWithKeyWord', params: { text } }
+        ],
+        _ClientVersion: 'js3.4.1',
+        _ApplicationId: 'E62VyFVLMiW7kvbtVq3p',
+        g_os: 'PCWeb',
+        g_ver: 'v4.9.3.20241122',
+        _InstallationId: '0e1d6b85-f10e-4c00-bee4-9f11be8cc379'
     };
 
     const res = await fetchData(url, {
@@ -23,14 +30,21 @@ export const translate = async ({ text }: TranslateParams) => {
     try {
         const data = await res.json();
 
-        if (data.result.words.length === 0) { throw getError(NO_RESULT); }
+        if (data.result.code !== 200) { throw `Error Code ${data.result.code}`; }
+
+        const r = data.result.results['search-all'].result;
+
+        if (r.word.searchResult.length === 0) { throw getError(NO_RESULT); }
+
+        const hiragana = r.word.searchResult[0].title.split('|')[1]?.replace(/[\P{L}]/ug, '');
+        const hiraganaDict = hiragana ? [`[平假名] ${hiragana}`] : [];
 
         const result: TranslateResult = {
             text,
             from: '',
             to: 'ja',
-            result: [data.result.words[0].spell],
-            dict: [data.result.words[0].excerpt, `[平假名] ${data.result.words[0].pron}`, `[罗马音] ${data.result.words[0].romaji}`]
+            result: [r.word.searchResult[0].title.split('|')[0]],
+            dict: [r.word.searchResult[0].excerpt].concat(hiraganaDict)
         };
 
         return result;
