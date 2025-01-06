936efb73f1f5021568800e6cafedad75547637e8
sweetcola <838689894@qq.com>
2025-01-06 14:39:55 +0800

    Fix can't listen audio through popup button when default audio source is Google
    Closes #183

--- src/public/translate/google/detect.ts
+++ src/public/translate/google/detect.ts
@@ -1,15 +1,22 @@
 import { getQueryString, fetchData, getError } from '../utils';
 import { RESULT_ERROR } from '../error-codes';
 import { DetectParams } from '../translate-types';
+import { getTk } from '../../web-page-translate/google/getTk';
 
 export const detect = async ({ text }: DetectParams): Promise<string> => {
     let url = `https://translate.googleapis.com/translate_a/single`;
 
     let params = {
-        client: 'gtx',
+        client: 'webapp',
         sl: 'auto',
+        tl: 'en',
+        hl: 'en',
+        dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+        ie: 'UTF-8',
+        oe: 'UTF-8',
         dj: 1,
-        q: encodeURIComponent(text)
+        q: encodeURIComponent(text),
+        tk: getTk(text)
     };
 
     url += getQueryString(params);
