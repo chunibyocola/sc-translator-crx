b3cb7aed52da9b7ebb33938ebe7987120d45bf28
sweetcola <838689894@qq.com>
2025-03-05 16:07:40 +0800

    Able to set position as "fixed" on "SelectOptions"

--- src/components/SelectOptions/index.tsx
+++ src/components/SelectOptions/index.tsx
@@ -3,10 +3,17 @@ import { useWindowSize } from '../../public/react-use';
 import { cn } from '../../public/utils';
 import './style.css';
 
-const calculateSelectOptionsStyle = (relativeElement: HTMLElement | null | undefined, optionsMaxHeight: number, optionsMaxWidth: number, cover?: boolean) => {
+const calculateSelectOptionsStyle = (relativeElement: HTMLElement | null | undefined, optionsMaxHeight: number, optionsMaxWidth: number, fixed: boolean, cover?: boolean) => {
     if (!relativeElement) { return {}; }
 
-    const { top: relativeTop, height: relativeHeight, right: relativeRight, width: relativeWidth, bottom: relativeBottom } = relativeElement.getBoundingClientRect();
+    const {
+        top: relativeTop,
+        height: relativeHeight,
+        left: relativeLeft,
+        right: relativeRight,
+        width: relativeWidth,
+        bottom: relativeBottom
+    } = relativeElement.getBoundingClientRect();
     const documentWidth = document.documentElement.clientWidth, documentHeight = document.documentElement.clientHeight;
 
     const verticalRelativeDistance = cover ? 0 : relativeHeight;
@@ -25,6 +32,42 @@ const calculateSelectOptionsStyle = (relativeElement: HTMLElement | null | undef
         maxWidth: Math.min(optionsMaxWidth, documentWidth - 10),
         width: optionsMaxWidth
     };
+
+    if (fixed) {
+        nextStyle.left = undefined;
+        nextStyle.maxHeight = optionsMaxHeight;
+
+        if (relativeBottom + optionsMaxHeight + 5 <= documentHeight) {
+            nextStyle.top = relativeBottom;
+        }
+        else if (relativeTop - 5 >= optionsMaxHeight) {
+            nextStyle.bottom = documentHeight - relativeTop;
+        }
+        else if (documentHeight - relativeBottom >= relativeTop) {
+            nextStyle.top = relativeBottom;
+            nextStyle.maxHeight = documentHeight - relativeBottom - 5;
+        }
+        else {
+            nextStyle.bottom = documentHeight - relativeTop;
+            nextStyle.maxHeight = relativeTop - 5;
+        }
+
+        if (relativeLeft + optionsMaxWidth + 5 <= documentWidth) {
+            nextStyle.left = relativeLeft;
+        }
+        else if (optionsMaxWidth + 10 <= documentWidth) {
+            nextStyle.left = documentWidth - 5 - optionsMaxWidth;
+        }
+        else {
+            optionsMaxWidth = documentWidth - 10;
+            nextStyle.left = 5;
+        }
+
+        nextStyle.maxWidth = optionsMaxWidth;
+        nextStyle.width = optionsMaxWidth;
+
+        return nextStyle;
+    }
 
     if (bottomGap >= optionsMaxHeight) {
         nextStyle.maxHeight = Math.min(optionsMaxHeight, bottomGap);
@@ -59,6 +102,7 @@ type SelectOptionsProps = {
     maxHeight?: number;
     maxWidth?: number;
     show: boolean;
+    fixed?: boolean;
     cover?: boolean;
     onShow?: () => void;
 } & Pick<React.HTMLAttributes<HTMLDivElement>, 'children' | 'style' | 'className' | 'onMouseLeave' | 'onMouseDown' | 'onClick'>;
@@ -72,6 +116,7 @@ const SelectOptions = React.forwardRef<SelectOptionsForwardRef, SelectOptionsPro
     maxHeight = 300,
     maxWidth = 200,
     show,
+    fixed,
     cover,
     style,
     className,
@@ -91,8 +136,8 @@ const SelectOptions = React.forwardRef<SelectOptionsForwardRef, SelectOptionsPro
     }));
 
     useLayoutEffect(() => {
-        show && setOptionsStyle(calculateSelectOptionsStyle(optionsElementRef.current?.parentElement, maxHeight, maxWidth, cover));
-    }, [show, maxHeight, maxWidth, windowSize, cover]);
+        show && setOptionsStyle(calculateSelectOptionsStyle(optionsElementRef.current?.parentElement, maxHeight, maxWidth, !!fixed, cover));
+    }, [show, maxHeight, maxWidth, windowSize, cover, fixed]);
 
     useLayoutEffect(() => {
         show && onShow?.();
@@ -102,7 +147,7 @@ const SelectOptions = React.forwardRef<SelectOptionsForwardRef, SelectOptionsPro
         <div
             className={cn('select-options', className)}
             ref={optionsElementRef}
-            style={Object.assign({ display: show ? 'block' : 'none' }, optionsStyle, style)}
+            style={Object.assign({ display: show ? 'block' : 'none' }, { position: fixed ? 'fixed' : 'absolute' }, optionsStyle, style)}
             onMouseLeave={onMouseLeave}
             onMouseDown={onMouseDown}
             onClick={onClick}
