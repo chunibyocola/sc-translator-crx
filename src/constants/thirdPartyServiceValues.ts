b4c34ee24552695a60eccf284d8c880aa89f2be9
sweetcola <838689894@qq.com>
2025-08-21 18:03:43 +0800

    Move commonPrompt into i18n

--- src/constants/thirdPartyServiceValues.ts
+++ src/constants/thirdPartyServiceValues.ts
@@ -1,12 +1,10 @@
 import { TPSGeminiValue } from '../types/thirdPartyValue';
 
-const commonPrompt = 'Translate the following text into {targetLanguage}:\n\n{text}';
-
 export const defaultGeminiValue: Required<TPSGeminiValue> = {
     name: 'Gemini',
     url: 'https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent',
     model: 'gemini-2.0-flash',
-    prompt: commonPrompt,
+    prompt: '',
     key: ''
 };
 
