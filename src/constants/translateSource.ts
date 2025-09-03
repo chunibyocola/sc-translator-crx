1b595464ee5af69e2a0cf025990a957e02ba3ee9
sweetcola <838689894@qq.com>
2025-09-03 14:48:48 +0800

    Add translation source "Browser AI"

--- src/constants/translateSource.ts
+++ src/constants/translateSource.ts
@@ -3,6 +3,7 @@ export const BING_COM = 'bing.com';
 export const BAIDU_COM = 'baidu.com';
 export const MOJIDICT_COM = 'mojidict.com';
 export const MICROSOFT_COM = 'microsofttranslator.com';
+export const BROWSER_AI = 'browser_ai';
 
 export type TranslateSource = {
     source: string;
@@ -14,7 +15,8 @@ export const translateSource: TranslateSource[] = [
     { source: GOOGLE_COM, url: 'translate.google.com' },
     { source: BING_COM, url: 'www.bing.com/translator/' },
     { source: BAIDU_COM, url: 'fanyi.baidu.com' },
-    { source: MOJIDICT_COM, url: 'www.mojidict.com' }
+    { source: MOJIDICT_COM, url: 'www.mojidict.com' },
+    { source: BROWSER_AI, url: 'browser.ai' }
 ];
 
 export const audioSource: TranslateSource[] = [
